4420d50afd343009a8fd9b727aff448c51987bd7
Victor Gaydov <victor@enise.org>
2016-06-20 12:07:30 +0300

    Update package doc

--- monotime.go
+++ monotime.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Apache License 2.0
 // that can be found in the COPYING file.
 
-// Package monotime exports monotonic clock function from Go runtime package.
+// Package monotime provides functions to access monotonic clock source.
 package monotime
 
 import (
